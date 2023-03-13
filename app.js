149d6c7b706dab1035fe84425230feea36ee950f
eakkarunpoom <eakkarunpoom.ta@gmail.com>
2023-03-13 14:43:54 +0700

    test tenary

--- app.js
+++ app.js
@@ -1,25 +1,31 @@
-let dayNumber = 0;
-if (dayNumber === 0) {
-    console.log('Sunday');
-}
-else if (dayNumber === 1) {
-    console.log('Monday');
-}
-else if (dayNumber === 2) {
-    console.log('Tuesday');
-}
-else if (dayNumber === 3) {
-    console.log('Wednesday');
-}
-else if (dayNumber === 4) {
-    console.log('Thursday');
-}
-else if (dayNumber === 5) {
-    console.log('Friday');
-}
-else if (dayNumber === 6) {
-    console.log('Saturday');
-}
-else {
-    console.log("Invalid day number")
-} 
+// let dayNumber = 0;
+// if (dayNumber === 0) {
+//     console.log('Sunday');
+// }
+// else if (dayNumber === 1) {
+//     console.log('Monday');
+// }
+// else if (dayNumber === 2) {
+//     console.log('Tuesday');
+// }
+// else if (dayNumber === 3) {
+//     console.log('Wednesday');
+// }
+// else if (dayNumber === 4) {
+//     console.log('Thursday');
+// }
+// else if (dayNumber === 5) {
+//     console.log('Friday');
+// }
+// else if (dayNumber === 6) {
+//     console.log('Saturday');
+// }
+// else {
+//     console.log("Invalid day number")
+// } 
+
+
+//ternary 
+const password = 'pas$word';
+password.length >= 12 ? console.log('password is very strong so WELCOME!!!') : password.length >= 8 ? console.log('WELCOME!!!') : console.log('SORRY!!! the password is too short');
+
